491a66e010c31825e4e25fbf7ff2b952301fc51d
Jason <18272164317@163.com>
2021-05-26 15:51:18 +0800

    nothing changed

---
+++ app/src/main/java/com/example/frameworklib/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.frameworklib;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author：Jason
+ * @date: 2021/5/26 15:00
+ * @email：1129847330@qq.com
+ * @description:
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
